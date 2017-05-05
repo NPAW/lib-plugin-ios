3de3942e0c0cac4822c6a48c5a7c284bfa1e8d17
Joan Treveset <jtreveset@nicepeopleatwork.com>
2017-05-05 10:55:33 +0200

    CI: Mirror repo also when tagging

---
+++ scripts/install_git_and_mirror.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get install -y git
+mkdir -p ~/.ssh
+(umask 077; echo $QA_SSH_KEY | base64 --decode > ~/.ssh/id_rsa)          
+git push --prune git@github.com:NPAW/lib-plugin-ios.git +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
