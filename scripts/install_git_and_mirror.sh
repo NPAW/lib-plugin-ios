620ca12f542271488c64e5f3f84f3690fbe4d143
Enrique Alfonso Burillo <enrique@nicepeopleatwork.com>
2019-03-08 17:12:05 +0900

    Update mirror git script

--- scripts/install_git_and_mirror.sh
+++ scripts/install_git_and_mirror.sh
@@ -2,4 +2,4 @@ apt-get update
 apt-get install -y git
 mkdir -p ~/.ssh
 (umask 077; echo $QA_SSH_KEY | base64 --decode > ~/.ssh/id_rsa)          
-git push --mirror --all git@github.com:NPAW/lib-plugin-ios.git
+git push --all git@github.com:NPAW/lib-plugin-ios.git
