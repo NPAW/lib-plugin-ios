87816c84897f656efffa4826837bae2cdf54d0e2
Elisabet Massó <elisabet.masso@nicepeopleatwork.com>
2021-06-30 13:13:22 +0200

    hls parser and test case for complex manifests

--- YouboraLibTests/comm/transform/resourceparse/YBHlsParserTest.swift
+++ YouboraLibTests/comm/transform/resourceparse/YBHlsParserTest.swift
@@ -79,6 +79,38 @@ import XCTest
         XCTAssertTrue(parsedResource == expectedFinalResourceUrl)
     }
     
+    func testComplexResourceParse() {
+        let resourceUrl = "https://weirdurl.com/abcd/def.ts?testtoken=123456"
+        
+        var responseString = """
+        #EXTM3U\n\
+        #EXT-X-VERSION:4\n\
+        #EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio-0",NAME="en (Main)",DEFAULT=YES,AUTOSELECT=YES,LANGUAGE="en",URI="https://weirdurl.com/abcd/def.ts?testtoken=123456"\n
+        """
+
+        let parser = YBHlsParser()
+        _ = parser.isSatisfied(resource: resourceUrl, manifest: responseString.data(using: .utf8))
+
+        var parsedResource = parser.parseResource(data: responseString.data(using: .utf8), response: nil, listenerParents: nil)
+
+        XCTAssertTrue(parsedResource == "https://weirdurl.com/abcd/def.ts?testtoken=123456")
+        
+        responseString = """
+        #EXTM3U\n\
+        #EXT-X-TARGETDURATION:10\n\
+        #EXT-X-MEDIA-SEQUENCE:0\n\
+        #EXTINF:10,\n\
+        0640/06400.ts\n\
+        #EXTINF:10,\n\
+        0640/06401.ts\n
+        """
+        
+        parsedResource = parser.parseResource(data: responseString.data(using: .utf8), response: nil, listenerParents: nil)
+
+        let expectedFinalResourceUrl = "https://weirdurl.com/abcd/0640/06400.ts"
+        XCTAssertTrue(parsedResource == expectedFinalResourceUrl)
+    }
+    
     func testHostFinalResource() {
         let resourceUrl = "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8"
         
