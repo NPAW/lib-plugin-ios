101726823ec0a47db475d1dcf8254f7f8d2d1d54
Elisabet Massó <elisabet.masso@nicepeopleatwork.com>
2021-10-04 10:39:55 +0200

    bump version to 6.6.3

--- YouboraLib/YBConstants.swift
+++ YouboraLib/YBConstants.swift
@@ -278,7 +278,7 @@ import Foundation
     static public let jsInjectionSessionRootNotification = "jsInjectionSessionRootNotification"
     
     // Lib version
-    static public var youboraLibVersion = "6.6.2"
+    static public var youboraLibVersion = "6.6.3"
     
     static public let preferencesSessionIdKey = "session_id"
     static public let preferencesContextKey = "context_id"
