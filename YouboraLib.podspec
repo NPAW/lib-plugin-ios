bad00fdf710dfbe284c510d654ad7c6ab01493f0
Elisabet Massó <elisabet.masso@nicepeopleatwork.com>
2022-07-08 15:18:51 +0200

    bump version to 6.6.20

--- YouboraLib.podspec
+++ YouboraLib.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = 'YouboraLib'
-  s.version = '6.6.19'
+  s.version = '6.6.20'
 
 
   # Metadata
--- YouboraLib/YBConstants.swift
+++ YouboraLib/YBConstants.swift
@@ -281,7 +281,7 @@ import Foundation
     static public let jsInjectionSessionRootNotification = "jsInjectionSessionRootNotification"
     
     // Lib version
-    static public var youboraLibVersion = "6.6.18"
+    static public var youboraLibVersion = "6.6.20"
     
     static public let preferencesSessionIdKey = "session_id"
     static public let preferencesContextKey = "context_id"
